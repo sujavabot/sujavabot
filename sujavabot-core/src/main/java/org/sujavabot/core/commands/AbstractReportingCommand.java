db68c4b5143bc7bdcc0ceda2fdd27b1f382b08f8
robin <robin.kirkman@gmail.com>
2015-07-03 05:28:36 -0400

    Protected method to apply prefix

--- sujavabot-core/src/main/java/org/sujavabot/core/commands/AbstractReportingCommand.java
+++ sujavabot-core/src/main/java/org/sujavabot/core/commands/AbstractReportingCommand.java
@@ -1,5 +1,6 @@
 package org.sujavabot.core.commands;
 
+import org.pircbotx.User;
 import org.pircbotx.hooks.Event;
 import org.pircbotx.hooks.events.ActionEvent;
 import org.pircbotx.hooks.events.MessageEvent;
@@ -9,17 +10,21 @@ import org.sujavabot.core.SujavaBot;
 
 public abstract class AbstractReportingCommand implements Command {
 
+	protected String prefix(User user, String result) {
+		return user.getNick() + ": " + result;
+	}
+	
 	@Override
 	public void report(SujavaBot bot, Event<?> cause, String result) {
 		if(cause instanceof MessageEvent<?>) {
 			MessageEvent<?> m = (MessageEvent<?>) cause;
-			m.getChannel().send().message(m.getUser(), result);
+			m.getChannel().send().message(prefix(m.getUser(), result));
 		} else if(cause instanceof ActionEvent<?>) {
 			ActionEvent<?> a = (ActionEvent<?>) cause;
-			a.getChannel().send().message(a.getUser(), result);
+			a.getChannel().send().message(prefix(a.getUser(), result));
 		} else if(cause instanceof PrivateMessageEvent<?>) {
 			PrivateMessageEvent<?> p = (PrivateMessageEvent<?>) cause;
-			p.getUser().send().message(result);
+			p.getUser().send().message(prefix(p.getUser(), result));
 		}
 	}
 
