bd124d21237079ee83c1105d480cb300f9dc6294
robin <robin.kirkman@gmail.com>
2015-07-03 13:29:31 -0400

    Fix parse error command

--- sujavabot-core/src/main/java/org/sujavabot/core/commands/ParseErrorCommand.java
+++ sujavabot-core/src/main/java/org/sujavabot/core/commands/ParseErrorCommand.java
@@ -9,7 +9,7 @@ public class ParseErrorCommand extends AbstractReportingCommand {
 
 	@Override
 	public String invoke(SujavaBot bot, Event<?> cause, List<String> args) {
-		return "Parse error for input: " + args.get(0);
+		return "Parse error for input: " + args.get(1);
 	}
 
 }
