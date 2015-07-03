d3e18e5486047bea1d3587eb5e5aae3540fd18a9
robin <robin.kirkman@gmail.com>
2015-07-03 03:39:59 -0400

    CommandReceiverListener

---
+++ sujavabot-core/src/main/java/org/sujavabot/core/commands/CommandReceiverListener.java
@@ -0,0 +1,34 @@
+package org.sujavabot.core.commands;
+
+import org.pircbotx.PircBotX;
+import org.pircbotx.hooks.ListenerAdapter;
+import org.pircbotx.hooks.events.ActionEvent;
+import org.pircbotx.hooks.events.MessageEvent;
+import org.pircbotx.hooks.events.PrivateMessageEvent;
+import org.sujavabot.core.SujavaBot;
+
+public class CommandReceiverListener extends ListenerAdapter<PircBotX> {
+	protected String prefix;
+	
+	public CommandReceiverListener(String prefix) {
+		this.prefix = prefix;
+	}
+	
+	@Override
+	public void onMessage(MessageEvent<PircBotX> event) throws Exception {
+		String m = event.getMessage();
+		if(!m.startsWith(prefix))
+			return;
+		m = m.substring(prefix.length());
+		((SujavaBot) event.getBot()).getCommands().perform(event, m);
+	}
+	
+	@Override
+	public void onPrivateMessage(PrivateMessageEvent<PircBotX> event) throws Exception {
+		String m = event.getMessage();
+		if(!m.startsWith(prefix))
+			return;
+		m = m.substring(prefix.length());
+		((SujavaBot) event.getBot()).getCommands().perform(event, m);
+	}
+}
