f31ff98c6664481111391b7004aec5bbbe58c06b
robin <robin.kirkman@gmail.com>
2015-09-15 15:04:23 -0400

    Strip the prompt

--- sujavabot-plugin-markov/src/main/java/org/sujavabot/plugin/markov/MarkovListener.java
+++ sujavabot-plugin-markov/src/main/java/org/sujavabot/plugin/markov/MarkovListener.java
@@ -61,7 +61,7 @@ public class MarkovListener extends ListenerAdapter<PircBotX> {
 				return;
 			MarkovIterator mi = new MarkovIterator(markov, maxlen, prefix);
 			List<String> ml = mi.toList();
-//			ml.subList(0, prefix.size()).clear();
+			ml.subList(0, prefix.size()).clear();
 			for(int i = ml.size() - 3; i >= 0; i--) {
 				int j = i+3;
 				List<String> sub = ml.subList(i, j);
