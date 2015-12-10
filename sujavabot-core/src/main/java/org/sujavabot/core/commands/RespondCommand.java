6968de110e68cd5a8800b507d78de0fb00a6815c
robin <robin.kirkman@gmail.com>
2015-12-10 03:11:43 -0500

    respond command

---
+++ sujavabot-core/src/main/java/org/sujavabot/core/commands/RespondCommand.java
@@ -0,0 +1,29 @@
+package org.sujavabot.core.commands;
+
+import java.util.List;
+import java.util.Map;
+
+import org.pircbotx.hooks.Event;
+import org.pircbotx.hooks.events.MessageEvent;
+import org.sujavabot.core.SujavaBot;
+
+public class RespondCommand extends AbstractReportingCommand {
+	
+	@Override
+	protected Map<String, String> helpTopics() {
+		return buildHelp("respond with text");
+	}
+	
+	@Override
+	public String invoke(SujavaBot bot, Event<?> cause, List<String> args) {
+		StringBuilder sb = new StringBuilder();
+		String sep = "";
+		for(String arg : args.subList(1, args.size())) {
+			sb.append(sep);
+			sb.append(arg);
+			sep = " ";
+		}
+		return sb.toString();
+	}
+
+}
