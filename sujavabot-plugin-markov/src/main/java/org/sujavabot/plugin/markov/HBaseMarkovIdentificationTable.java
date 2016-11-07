c99e083681ceefb3bdefd167c06770d123dfcd3f
robin <robin@robin-i7>
2016-11-07 02:15:40 -0500

    identification table based on existing markov tables

---
+++ sujavabot-plugin-markov/src/main/java/org/sujavabot/plugin/markov/HBaseMarkovIdentificationTable.java
@@ -0,0 +1,139 @@
+package org.sujavabot.plugin.markov;
+
+import java.io.Flushable;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Properties;
+import java.util.TreeMap;
+import java.util.function.Function;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.client.Get;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Increment;
+import org.apache.hadoop.hbase.client.Result;
+import org.apache.hadoop.hbase.client.Scan;
+import org.apache.hadoop.hbase.client.Table;
+import org.apache.hadoop.hbase.util.Bytes;
+
+public class HBaseMarkovIdentificationTable implements IdentificationTable {
+	public static final String DEFAULT_FAMILY = "ident-suffix";
+	public static final double DEFAULT_DISTANCE_POWER = 3.;
+	
+	protected static byte[] escapeBytes(byte[] b) {
+		return Bytes.toBytes(Bytes.toStringBinary(b));
+	}
+	
+	protected static byte[] unescapeBytes(byte[] b) {
+		return Bytes.toBytesBinary(Bytes.toString(b));
+	}
+	
+	protected static byte[] familyToBytes(String family) {
+		return escapeBytes(Bytes.toBytes(family));
+	}
+	
+	protected static String bytesToFamily(byte[] b) {
+		return Bytes.toString(unescapeBytes(b));
+	}
+	
+	protected Properties properties;
+	protected Table table;
+	protected byte[] family;
+	protected double distancePower;
+
+	public HBaseMarkovIdentificationTable() {
+	}
+	
+	public HBaseMarkovIdentificationTable(Table table, String family) {
+		this(table, family, DEFAULT_DISTANCE_POWER);
+	}
+	
+	public HBaseMarkovIdentificationTable(Table table, String family, double distancePower) {
+		this.table = table;
+		this.family = familyToBytes(family);
+		this.distancePower = distancePower;
+	}
+	
+	@Override
+	public void put(long timestamp, byte[] prefix, byte[] suffix, byte[] id) throws IOException {
+		throw new UnsupportedOperationException();
+	}
+	
+	@Override
+	public Map<byte[], Double> get(long timestamp, byte[] prefix, byte[] suffix) throws IOException {
+		Get get = new Get(prefix);
+		get.addFamily(family);
+		
+		Map<byte[], Double> freqs = new TreeMap<>(Bytes.BYTES_COMPARATOR);
+		Map<byte[], Double> distances = new TreeMap<byte[], Double>(Bytes.BYTES_COMPARATOR);
+		
+		EditDistancer distancer = new EditDistancer();
+		Function<byte[], Double> distanceFn = (b) -> {
+			double editDistance = 0;
+			byte[] edits = distancer.compute(suffix, b);
+			for(int i = 0; i < edits.length; i++) {
+				if(edits[i] == EditDistancer.Op.NEXT)
+					editDistance += 1. / edits.length;
+			}
+			return Math.pow(editDistance, distancePower);
+		};
+
+		double normalizedTotal = 0;
+		Result result = table.get(get);
+		
+		for(Entry<byte[], byte[]> e : result.getFamilyMap(family).entrySet()) {
+			String[] f = Bytes.toString(e.getKey()).split(" ", 2);
+			byte[] resultSuffix = Bytes.toBytes(f[0]);
+			byte[] resultId = Bytes.toBytes(f[1]);
+			long count = Bytes.toLong(e.getValue());
+			
+			double idf = freqs.getOrDefault(resultId, 0.);
+			double d = count * distances.computeIfAbsent(resultSuffix, distanceFn);
+			normalizedTotal += d;
+			freqs.put(resultId, idf + d);
+		}
+
+		double nt = normalizedTotal;
+		freqs.replaceAll((k, v) -> v / nt);
+		
+		return freqs;
+	}
+
+	public Table getTable() {
+		return table;
+	}
+
+	@Override
+	public void flush() throws IOException {
+	}
+
+	public void setTable(Table table) {
+		this.table = table;
+	}
+
+	public String getFamily() {
+		return bytesToFamily(family);
+	}
+
+	public void setFamily(String family) {
+		this.family = familyToBytes(family);
+	}
+
+	public double getDistancePower() {
+		return distancePower;
+	}
+
+	public void setDistancePower(double distancePower) {
+		this.distancePower = distancePower;
+	}
+
+	public Properties getProperties() {
+		return properties;
+	}
+
+	public void setProperties(Properties conf) {
+		this.properties = conf;
+	}
+}
---
+++ sujavabot-plugin-markov/src/main/java/org/sujavabot/plugin/markov/HBaseMarkovIdentificationTableConverter.java
@@ -0,0 +1,97 @@
+package org.sujavabot.plugin.markov;
+
+import java.util.Properties;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.TableName;
+import org.apache.hadoop.hbase.client.Connection;
+import org.apache.hadoop.hbase.client.ConnectionFactory;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Table;
+import org.sujavabot.core.util.Throwables;
+import org.sujavabot.core.xml.AbstractConverter;
+import org.sujavabot.core.xml.ConverterHelpers.MarshalHelper;
+import org.sujavabot.core.xml.ConverterHelpers.UnmarshalHelper;
+import org.sujavabot.core.xml.XStreams;
+
+import com.thoughtworks.xstream.XStream;
+import com.thoughtworks.xstream.converters.ConversionException;
+import com.thoughtworks.xstream.converters.UnmarshallingContext;
+import com.thoughtworks.xstream.io.HierarchicalStreamReader;
+
+public class HBaseMarkovIdentificationTableConverter extends AbstractConverter<HBaseMarkovIdentificationTable> {
+	
+	public static final String TABLE = "table";
+	public static final String FAMILY = "family";
+
+	public static class SPI extends XStreams.SPI {
+		@Override
+		public void configure(XStream x) {
+			x.registerConverter(new HBaseMarkovIdentificationTableConverter(x));
+		}
+	}
+
+	public HBaseMarkovIdentificationTableConverter(XStream x) {
+		super(x, HBaseMarkovIdentificationTable.class);
+	}
+	
+	@Override
+	protected HBaseMarkovIdentificationTable createDefaults(HBaseMarkovIdentificationTable current) {
+		return null;
+	}
+
+	@Override
+	protected void configure(HBaseMarkovIdentificationTable current, MarshalHelper helper, HBaseMarkovIdentificationTable defaults) {
+		Properties props = new Properties();
+		if(current.getProperties() != null)
+			props.putAll(current.getProperties());
+		
+		props.setProperty(TABLE, current.getTable().getName().getNameAsString());
+		props.setProperty(FAMILY, current.getFamily());
+		
+		for(String key : props.stringPropertyNames()) {
+			helper.field(key, String.class, () -> props.getProperty(key));
+		}
+	}
+
+	@Override
+	protected void configure(HBaseMarkovIdentificationTable current, UnmarshalHelper helper) {
+	}
+
+	@Override
+	public Object unmarshal(HierarchicalStreamReader reader, UnmarshallingContext context) {
+		try {
+			HBaseMarkovIdentificationTable idtable = new HBaseMarkovIdentificationTable();
+			Properties props = new Properties();
+			
+			UnmarshalHelper helper = new UnmarshalHelper(x, reader, context);
+
+			helper.setDefaultHandler((Object o, UnmarshalHelper h) -> props.setProperty(h.getReader().getNodeName(), h.getReader().getValue()));
+			
+			helper.read(idtable);
+			
+			String table = (String) props.remove(TABLE);
+			String family = (String) props.remove(FAMILY);
+			
+			idtable.setProperties(props);
+			
+			Configuration conf = new Configuration();
+			
+			for(String key : props.stringPropertyNames()) {
+				conf.set(key, props.getProperty(key));
+			}
+
+			TableName tableName = TableName.valueOf(table);
+			
+			Connection hcxn = ConnectionFactory.createConnection(conf);
+			Table htable = hcxn.getTable(tableName);
+			
+			idtable.setTable(htable);
+			idtable.setFamily(family);
+			
+			return idtable;
+		} catch(Exception e) {
+			throw Throwables.as(ConversionException.class, e);
+		}
+	}
+}
