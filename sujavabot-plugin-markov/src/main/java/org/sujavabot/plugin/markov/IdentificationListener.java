3586fd92c2656a989d2a3c5ed0d537438f3a02c9
robin <robin@robin-i7>
2016-11-07 01:52:10 -0500

    listen to actions too

--- sujavabot-plugin-markov/src/main/java/org/sujavabot/plugin/markov/IdentificationListener.java
+++ sujavabot-plugin-markov/src/main/java/org/sujavabot/plugin/markov/IdentificationListener.java
@@ -7,6 +7,7 @@ import java.util.regex.Pattern;
 
 import org.pircbotx.PircBotX;
 import org.pircbotx.hooks.ListenerAdapter;
+import org.pircbotx.hooks.events.ActionEvent;
 import org.pircbotx.hooks.events.MessageEvent;
 
 public class IdentificationListener extends ListenerAdapter<PircBotX> {
@@ -14,6 +15,24 @@ public class IdentificationListener extends ListenerAdapter<PircBotX> {
 	protected Identification ident;
 	protected Set<String> channels;
 	protected Set<Pattern> ignore;
+	
+	@Override
+	public void onAction(ActionEvent<PircBotX> event) throws Exception {
+		if(channels != null && !channels.contains(event.getChannel().getName()))
+			return;
+		String m = event.getMessage();
+		m = event.getUser().getNick() + " " + m;
+		
+		List<String> content = StringContent.parse(m);
+		Iterator<String> ci = content.iterator();
+		while(ci.hasNext()) {
+			if(StringContent.LINK.matcher(ci.next()).matches())
+				ci.remove();
+		}
+		if(content.size() > 0) {
+			ident.consume(System.currentTimeMillis(), event.getUser().getNick(), content);
+		}
+	}
 	
 	@Override
 	public void onMessage(MessageEvent<PircBotX> event) throws Exception {
