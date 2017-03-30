de1f5e04e6b747199cbbf31d145e5b47add3d40b
Robin Kirkman <robin.kirkman@gmail.com>
2017-03-30 16:24:11 -0400

    flush but don't sync

--- sujavabot-core/src/main/java/org/sujavabot/core/commands/SeenCommand.java
+++ sujavabot-core/src/main/java/org/sujavabot/core/commands/SeenCommand.java
@@ -172,7 +172,6 @@ public class SeenCommand extends AbstractReportingCommand {
 				oout.writeObject(seen);
 				oout.close();
 				oout.flush();
-				out.getFD().sync();
 			} finally {
 				out.close();
 			}
